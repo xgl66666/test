fe1240a64f6f16a4e52408f20178c5a5ef438913
dusica <dusica.tomasevic@rt-rk.com>
2017-12-29 14:15:17 +0100

    tdal_common API changed in order to comply with GTPL tdal_common API

--- chal/tdal/inc/tdal_common.h
+++ chal/tdal/inc/tdal_common.h
@@ -156,10 +156,16 @@ typedef enum
 */
 typedef struct stTDAL_Diag_HWInfo
 {
+    char      cOUI[16];                    /* OUI */
+    uint32_t  uiOUIId;                     /* OUI Id */
     char      cHardwareVersion[16];        /* HardwareVersion */
     uint32_t  uiHardwareVersionId;         /* HardwareVersion Id */
     char      cHardwareModel[16];          /* HardwareModel */
     uint32_t  uiHardwareModelId;           /* HardwareModel Id */
+    char      cProductVersion[16];         /* ProductVersion */
+    uint32_t  uiProductVersionId;          /* ProductVersion Id*/
+    char      cProductModel[16];           /* ProductModel */
+    uint32_t  uiProductModelId;            /* ProductModel Id*/
     char      cHardwareDeviceName[16];     /* DeviceName */
     uint32_t  uiHardwareDeviceNameId;      /* DeviceName Id */
     char      cHardwareManufacturer[16];   /* Manufacturer */   
